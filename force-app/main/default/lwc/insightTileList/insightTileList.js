5ec2c36a5d86b89fc87e80677b8d8a362cbac5cb
Kavya Papasani <kavya.p@BRILLIO.COM>
2020-03-05 10:58:05 -0500

    LWC tabs

--- force-app/main/default/lwc/insightTileList/insightTileList.js
+++ force-app/main/default/lwc/insightTileList/insightTileList.js
@@ -5,8 +5,21 @@ export default class InsightTileList extends LightningElement {
 
     @track insights = []; 
     @track insight; 
+    @track iconuppriority; 
+    @track iconupbrand; 
+    @track iconupdate; 
 
     @wire(getInsights) 
     insights; 
+
+    sortbyPriority(){
+
+    }
+    sortbyBrand(){
+
+    }
+    sortbyDate(){
+        
+    }
 
 }
