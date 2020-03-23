10082544cff033e2d19d04a8bcc5e972164b0f68
Rahul Dey <rahul.dey@brillio.com>
2020-03-23 12:00:46 -0400

    Insights Changes & and DateTime render fix

--- force-app/main/default/lwc/highlights/highlights.js
+++ force-app/main/default/lwc/highlights/highlights.js
@@ -1,10 +1,11 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getHighlightsController from '@salesforce/apex/insightsController.getHighlightsController';
 
 export default class Highlights extends LightningElement
 {
     highlights;
     errors;
+    renderCards;
 
     @wire(getHighlightsController)
     wiredHighlights({ error, data }) {
@@ -12,11 +13,13 @@ export default class Highlights extends LightningElement
         {
             this.highlights = data;
             this.error = undefined;
+            this.renderCards=true;
         } 
         else if (error) 
         {
             this.error = error;
             this.highlights = undefined;
+            this.renderCards=false;
         }
     }
 }
